8814b39cf5dae379263c780d5a4e3669a0275a13
Juntaro Minezaki <16648225+mjdigit@users.noreply.github.com>
2025-09-14 22:25:36 +0900

    Added MacOS detection and increase layers to 8.

--- qmk_firmware/keyboards/lightseeker/keymaps/vial/keymap.c
+++ qmk_firmware/keyboards/lightseeker/keymaps/vial/keymap.c
@@ -1,2 +1,16 @@
 #include QMK_KEYBOARD_H
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {};
+
+void keyboard_post_init_user(void) {
+    wait_ms(400);
+
+    switch (detected_host_os()) {
+    case OS_MACOS:
+    case OS_IOS:
+        layer_move(4);
+        break;
+    default:
+        layer_move(0);
+        break;
+    }
+}
